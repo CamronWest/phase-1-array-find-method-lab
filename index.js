cd47163c5d88a4425d1addb1b4c4aa605d6a54c3
Camron <cawe219@gmail.com>
2023-01-11 12:45:49 -0500

    commit tree 9136d3d6f28885ed5c8021bf170e2065355704c7

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+
+
+
+
+
+
+// function findResults (){
+//   record.map((item) =>  {
+//     return item.result;
+//   }) 
+// }
+
+// const findYear = record.map((item) =>  {
+//   return item.year;
+// }) 
+
+superbowlWin = (record) => {
+  const result = record.find(record => record.result ==="W");
+  if (!!result === true){
+    return result.year
+  } else {
+    return undefined;
+  }
+}
+
+
